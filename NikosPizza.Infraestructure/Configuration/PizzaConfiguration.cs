5e8208818fb7e8541ab624858a0b1f088623de5b
Rosmel Merlo <rosmel2508@outlook.com.pe>
2024-11-08 23:00:12 -0500

    migration 1.0

--- NikosPizza.Infraestructure/Configuration/PizzaConfiguration.cs
+++ NikosPizza.Infraestructure/Configuration/PizzaConfiguration.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using NikosPizza.core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
+﻿
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NikosPizza.core.Entities;
 
 namespace NikosPizza.Infraestructure.Configuration
 {
---
+++ NikosPizza.Infraestructure/Migrations/20241109035941_initial1.0.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NikosPizza.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "pizza");
+
+            migrationBuilder.CreateTable(
+                name: "Pizzas",
+                schema: "pizza",
+                columns: table => new
+                {
+                    PizzaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tamaño = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<double>(type: "float", nullable: false),
+                    Creado = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Update = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizzas", x => x.PizzaId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pizzas",
+                schema: "pizza");
+        }
+    }
+}
